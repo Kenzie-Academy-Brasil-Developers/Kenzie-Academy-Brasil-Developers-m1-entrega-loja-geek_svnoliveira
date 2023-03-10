197e601ac019ff91353d12a256df7e216c27472b
samuel <srkuran@gmail.com>
2023-03-10 20:26:40 -0300

    removido tag Figure, traduzido todos termos para ingles

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-let itens = [
-    produto1 = {
+let items = [
+    product1 = {
         image: './assets/img/painting/clock.jpg',
         name: 'Smartwatch',
         price: 'R$100,00',
         type: 'Painting'
     },
-    produto2 = {
+    product2 = {
         image: './assets/img/painting/gamepad.jpg',
         name: 'Controle Playstation',
         price: 'R$100,00',
         type: 'Painting'
     },
-    produto3 = {
+    product3 = {
         image: './assets/img/painting/personagem.jpg',
         name: 'Robot',
         price: 'R$100,00',
         type: 'Painting'
     },
-    produto4 = {
+    product4 = {
         image: './assets/img/actions/animewoman.jpg',
         name: 'Anime Woman',
         price: 'R$100,00',
         type: 'Action Figures'
     },
-    produto5 = {
+    product5 = {
         image: './assets/img/actions/dragonballpersonagem.jpg',
         name: 'Goku Super Saiyajin',
         price: 'R$100,00',
         type: 'Action Figures'
     },
-    produto6 = {
+    product6 = {
         image: './assets/img/actions/starwarspersonagem.jpg',
         name: 'Sr Yoda',
         price: 'R$100,00',
@@ -41,7 +41,7 @@ let itens = [
 let listFigures = []                                        
 let listFrames = []                                         
 
-function separateItens (list){
+function separateitems (list){
     for (let i = 0; i < list.length; i++) {
         const element = list[i];
         if (element.type == 'Painting'){
@@ -51,38 +51,38 @@ function separateItens (list){
         }
     }
 }
-separateItens(itens);
+separateitems(items);
 
-let secaolistFigure = document.getElementById('paintings')
-let secaolistFrames = document.getElementById('actionFigures')
-console.log(secaolistFigure)
-console.log(secaolistFrames)
+let sectionListFigure = document.getElementById('paintings')
+let sectionListFrames = document.getElementById('actionFigures')
+console.log(sectionListFigure)
+console.log(sectionListFrames)
 
-function postItems(lista){
-    for(let i = 0; i < lista.length; i++){
-        let elementoLista = document.createElement('li')
+function postItems(itemList){
+    for(let i = 0; i < itemList.length; i++){
+        let elementItemList = document.createElement('li')
         let addImage = document.createElement('img')
         let textBox = document.createElement('div')                                 //criação dos elementos
-        let nome = document.createElement('span')
+        let itemName = document.createElement('span')
         let priceString = document.createElement('span')
 
-        addImage.src = `${lista[i].image}`
-        nome.innerText = `${lista[i].name}`
+        addImage.src = `${itemList[i].image}`
+        itemName.innerText = `${itemList[i].name}`
         textBox.classList.add('text-box')                                           //atribuição dos valores, e classes para o CSS
-        nome.classList.add('itemName')
-        priceString.innerText = `${lista[i].price}`
+        itemName.classList.add('itemName')
+        priceString.innerText = `${itemList[i].price}`
         priceString.classList.add('itemPrice')
 
-        elementoLista.appendChild(addImage)
-        textBox.appendChild(nome)                                                   //acoplando elementos nos seus lugares
+        elementItemList.appendChild(addImage)
+        textBox.appendChild(itemName)                                                   //acoplando elementos nos seus lugares
         textBox.appendChild(priceString)
-        elementoLista.appendChild(textBox)
+        elementItemList.appendChild(textBox)
         
-        if (lista[i].type == 'Action Figures'){
-            secaolistFigure.appendChild(elementoLista)                              //verificando a seção certa para acoplar o item finalizado
+        if (itemList[i].type == 'Action Figures'){
+            sectionListFigure.appendChild(elementItemList)                              //verificando a seção certa para acoplar o item finalizado
            }
-        if (lista[i].type == 'Painting'){
-            secaolistFrames.appendChild(elementoLista)
+        if (itemList[i].type == 'Painting'){
+            sectionListFrames.appendChild(elementItemList)
            }
 
       }
